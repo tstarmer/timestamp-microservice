73a1edeffb94d7eb731cb4353fa7cea4c0a21c73
Timothy Starmer <tim.starmer@gmail.com>
2017-02-24 14:41:03 +0000

    added moment.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-var unixRegEx = / /;
-var naturalRegEx= / /;
+var moment = require('moment');
+
+
+// var unixRegEx = / /;
+// var naturalRegEx= / /;
 var cleanSpaces = /%20/g;
 
 app.get('/', function(req,res){
-  res.send("App is running enter a date into the address bar 4")
+  res.send("App is running enter a date into the address bar 4");
 });
 
 app.get('/test', function(req,res){
@@ -67,37 +70,41 @@ app.get("/December%2015,%202015", function(req, res){
 })
 
 
-app.get('/*', function(req,res){
-  res.send(req.path);  
-  
-})
-
-
-// app.get('/*', function (req, res) {
-//   res.send(req);
-//   /*check req*/
-//   /*create new output*/
-//   var time = Date.parse(req.path);
-//   if(time){
-//     /*Time is valid*/
-//     var unixTime = time.getTime();/*to unix*/
-//     var naturalTime = time.toDateString(); /*to natural*/
-        
-//         console.log("unix time", unixTime);
-//         console.log("natural time", naturalTime);
-        
-//     var output = {
-//       "unix": unixTime,
-//       "natural": naturalTime
-//     }
-//     /*{ "unix": 1450137600, "natural": "December 15, 2015" }*/
-//     // console.log(JSON.stringify(output));
-//     res.send(output);  
-//   }
-  
-//   res.send(null);
+// app.get('/*', function(req,res){
+//   res.send(req.path);  
   
 // })
+
+
+/*Using Moment.js*/
+app.get('/*', function(req,res){
+  // check if valid
+  var reqTime = req.path.substr(1);//trim the /
+  
+  console.log(moment.unix(reqTime))
+  //not catching the unix
+  //not catching spaces
+  if(moment(reqTime).isValid()){
+    
+    // var parsedTime = moment(reqTime);
+    
+    //build output
+    var unixTime = moment(reqTime).format("X");
+    var naturalTime = moment(reqTime).format("MMMM DD, YYYY");  
+    
+    
+    var output = {
+      "unix": unixTime,
+      "natural": naturalTime
+    }
+    
+    res.send(output);
+  }
+  // not valid send nul
+  
+  res.send("not valid", null)
+})
+
 
 app.listen(process.env.PORT || 8080, function(){
   console.log('Example app listening on port ', process.env.PORT)
